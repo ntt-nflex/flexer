a0fc025d3dcf622c2dc17750f7ed4b702fd01da2
Georgi Dimitrov <georgijd@users.noreply.github.com>
2016-11-11 10:03:00 +0000

    Create post_metric_defintions.py
    Example of creating a metric definition in CMP and sending data points for it

---
+++ examples/post_metric_defintions.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+
+def get(event, context):
+    payload = {
+        'name': 'metric-name',
+        'unit': 'B',
+        'label': {
+            'en': {
+                'text': 'Description of the metric',
+                'specialisations': ['(%s)']
+            }
+        },
+        'downsampling-func': 'sum', 
+        'aggregation-func': 'mean',
+        'specialisation-func': 'max',
+    }
+    resp = context.api.post('/metric-definitions', payload)
+    print resp.status_code, resp.text
+    
+    data = {                                                                     
+        'resource_id': 'resource-UUID',                                              
+        'metrics': [{                                                 
+            'metric': 'metric-name',                                          
+            'unit': 'B',                                                     
+            'value': float(42),                                                   
+            'time': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),                                                   
+        }]
+    }  
+    resp = context.api.post('/metrics', data)
+    print resp.status_code, resp.text
