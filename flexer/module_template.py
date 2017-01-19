b554d7cad40fd3680a07e0be7c3049ed01822e26
tintoy <tintoy@tintoy.io>
2017-01-19 12:06:28 +0900

    Example code for module-type-specific template parameters.

--- flexer/module_template.py
+++ flexer/module_template.py
@@ -21,15 +21,16 @@ class ModuleTemplate(object):
         self.template_type = template_type
         self.template_dir = template_dir
 
-    def create_module(self, name, target_dir):
+    def create_module(self, client, name, target_dir):
         """
         Create a new nFlex module.
 
+        :param client: The CMP API client.
         :param name: The name for the new template
         :param target_dir: The directory in which the module will be created.
         """
 
-        print 'Create nFlex module in "{}" from "{}"'.format(self.template_dir, target_dir)
+        print "Creating nFlex module in '{}'...".format(self.template_dir, target_dir)
 
         for template_file in os.listdir(self.template_dir):
             if template_file.endswith('.j2'):
@@ -38,7 +39,7 @@ class ModuleTemplate(object):
 
                 with open(path.join(target_dir, template_file[:-3])) as target_file:
                     target_file.write(template.render(
-                        **self.get_template_params(name)
+                        **self.get_template_params(client, name)
                     ))
             else:
                 shutil.copyfile(
@@ -46,21 +47,54 @@ class ModuleTemplate(object):
                     path.join(target_dir, template_file)
                 )
 
-    def get_template_params(self, module_name):
+    def get_template_params(self, client, module_name):
+        """
+        Get interpolation parameters for Jinja2 templates.
+
+        :param client: The CMP API client.
+        :param module_name: The name of the module being created.
+        :return: A dict containing the template parameters.
+        """
+
         template_params = {
             'module_name': module_name
         }
 
-        # Template-type-specific parameters.
-        if self.template_type == "widget":
-            # template_params.update({ 'baz': 'bonk' })
-
-            pass
-        elif self.template_type == "metrics_connector":
-            # Look up valid account, resource, etc.
-
-            # template_params.update({ 'account_id': account_id, 'resource_id': resource_id })
-
-            pass
+        # Add template-type-specific parameters (if any).
+        self._add_parameters(template_params, client)
 
         return template_params
+
+    @staticmethod
+    def add_resource_connector_template_params(template_params, client):
+        """
+        Get additional template parameters for a resource connector template
+
+        :param template_params: The existing template parameters.
+        :param client: The CMP API client.
+        """
+
+        account = client.get("/accounts")[0]
+        template_params.update({
+            'account_id': account['id'],
+            'account_name': account['name'],
+            'provider_id': account['provider']['id'],
+            'provider_name': account['provider']['name']
+        })
+
+    def _add_parameters(self, template_params, client):
+        """
+        Add template-type-specific parameters (if any) for the current template type.
+
+        :param template_params: The existing template parameters.
+        :param client: The CMP API client.
+        """
+
+        additional_parameters = self.__class__.__dict__.get(
+            'add_{}_template_params'.format(self.template_type)
+        )
+        if additional_parameters:
+            if additional_parameters is staticmethod:
+                additional_parameters.__func__(template_params, client)
+            else:
+                additional_parameters(template_params, client)
